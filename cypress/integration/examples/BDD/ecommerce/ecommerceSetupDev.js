df758194d153f7de841b10f3c1f869ba3b4e564f
unknown <jatvivek658@gmail.com>
2024-03-20 00:09:09 +0530

    Updating

--- cypress/integration/examples/BDD/ecommerce/ecommerceSetupDev.js
+++ cypress/integration/examples/BDD/ecommerce/ecommerceSetupDev.js
@@ -5,23 +5,30 @@ Given("I open Bewkoof shoping Website", function () {
 });
 
 // Search product category
+function handleWzrkAlert() {
+    cy.get('.wzrk-alert').then(($alert) => {
+        if ($alert.length > 0) {
+            cy.get('.wzrk-alert').within(() => {
+                cy.get('#wzrk-cancel').click();
+            });
+        }
+        // } else {
+        //     cy.get('.searchInput').type('t-shirt');
+        //     cy.wait(4000);
+        //     cy.contains('Men').click();
+        // }
+    });
+}
 When('Search product category', () => {
     cy.wait(4000)
-    function handleWzrkAlert() {
-        cy.get('.wzrk-alert').then(($alert) => {
-            if ($alert.length > 0) {
-                cy.get('.wzrk-alert').within(() => {
-                    cy.get('#wzrk-cancel').click(); 
-                });
-            } else {
-                cy.get('.searchInput').type('t-shirt');
-                cy.wait(4000);
-                cy.contains('Men').click();
-            }
+
             handleWzrkAlert();
+    cy.get('.searchInput').type('t-shirt');
+        cy.wait(4000);
+        cy.contains('Men').click();
         });
-    }
-    });
+
+
 
 // Navigate to PLP
 When('Navigate to PLP', () => {
